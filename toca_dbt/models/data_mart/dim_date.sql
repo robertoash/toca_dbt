1bd952b0a0a49397098fa4ccdabead0e225410ba
rash <j.roberto.ash@gmail.com>
2025-02-25 11:06:34 +0100

    Fix dim_date naming

--- toca_dbt/models/data_mart/dim_date.sql
+++ toca_dbt/models/data_mart/dim_date.sql
@@ -5,9 +5,9 @@
 }}
 
 SELECT
-    event_date,
-    DATE_TRUNC(event_date, WEEK) AS event_week,
-    DATE_TRUNC(event_date, MONTH) AS event_month,
-    DATE_TRUNC(event_date, QUARTER) AS event_quarter,
-    DATE_TRUNC(event_date, YEAR) AS event_year
+    event_date AS date,
+    DATE_TRUNC(event_date, WEEK) AS date_week,
+    DATE_TRUNC(event_date, MONTH) AS date_month,
+    DATE_TRUNC(event_date, QUARTER) AS date_quarter,
+    DATE_TRUNC(event_date, YEAR) AS date_year
 FROM {{ ref('intm_all_events') }}
