46ad6d1d8b22e46ad276a09b5a9906dc907dc7c4
rash <j.roberto.ash@gmail.com>
2025-02-25 12:01:41 +0100

    Added exchange_rate_id to stg_exchange_rates

--- toca_dbt/models/staging/stg_exchange_rates.sql
+++ toca_dbt/models/staging/stg_exchange_rates.sql
@@ -6,6 +6,7 @@
 
 WITH exchange_rates AS (
     SELECT
+        {{ dbt_utils.generate_surrogate_key(['dt', 'currency_code']) }} AS exchange_rate_id,
         dt AS currency_exchange_date,
         currency_code,
         usd_per_currency,
